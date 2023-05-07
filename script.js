44ebca2cbb262c936be4e1d626faff1eb776fc60
Reza Abedi <reza.abd95@yahoo.com>
2023-05-08 03:06:11 +0430

    change if format

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function numberOfLetters(sentence){
 
 const data =  sentence.reduce((prev, cur) => {
-  if(!prev[cur]) prev[cur] = {letter: cur , number: 1};
-  else prev[cur].number = prev[cur].number+1;
+
+  if(!prev[cur]){
+    prev[cur] = {letter: cur , number: 1};
+  } 
+  else{
+    prev[cur].number = prev[cur].number+1;
+  } 
   return prev;
+
 }, {})
   return data;
 }
 
 
-let sentence = ('Hi my name is Reza ABD').split("");
-console.log(numberOfLetters(sentence));
+let mySentence = ('Hi my name is Reza ABD').split("");
+console.log(numberOfLetters(mySentence));
 
 
 
